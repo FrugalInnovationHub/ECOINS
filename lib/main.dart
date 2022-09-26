09ad6414dbd5de922f31f1dc3df262888140b00f
Gautam <gchitnis@scu.edu>
2022-09-26 13:58:49 -0700

    Added basic game flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'game.dart';
+import 'screens/main_menu.dart';
 
 void main() {
-  final game = EcoinsGame();
-  runApp(
-    GameWidget(
-      game: game,
-    ),
-  );
+  // final game = EcoinsGame();
+  // runApp(
+  //   GameWidget(
+  //     game: game,
+  //   ),
+  // );
+  
+  runApp(app());
+  
 }
+
+class app extends StatelessWidget {
+  const app({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ECOINS",
+      home: main_menu(),
+    );
+  }
+}
+
---
+++ lib/screens/main_menu.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ecoins/game.dart';
+import 'package:flame/game.dart';
+
+class main_menu extends StatelessWidget {
+  const main_menu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/cocina_blur.png"),
+              fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: Card(
+            color: Colors.black.withOpacity(0.4),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 100.0, vertical: 50.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'ECOINS',
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.white,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => GameWidget(
+                                  game: EcoinsGame(),
+                                )));
+                        // gameRef.startGamePlay();
+                        // gameRef.overlays.remove(MainMenu.id);
+                        // gameRef.overlays.add(Hud.id);
+                      },
+                      child: const Text(
+                        'Play',
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

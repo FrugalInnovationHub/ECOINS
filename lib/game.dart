e3b1ed3e2b79456e4c6762f7f1ac57dfcaae479e
Abhishek Shukla <shukla1996abhishek@gmail.com>
2022-10-30 23:51:05 -0700

    updated dev code

---
+++ lib/game.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+import 'dart:io';
+
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+
+import 'cocina.dart';
+import 'banda_t.dart';
+import 'trash_items.dart';
+import 'banda_t_hole.dart';
+import 'basureros.dart';
+
+class EcoinsGame extends FlameGame with HasTappables, HasCollisionDetection{
+  Cocina _cocina = Cocina();
+  Banda_T _banda_t = Banda_T();
+  Botella_Plastico _bp = Botella_Plastico();
+  Basureros _basureros = Basureros();
+
+  var hole_pos = [
+    Vector2(850, 100),
+    Vector2(350, 200),
+    Vector2(700, 300)
+  ];
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    // debugMode = true;
+    await add(_cocina);
+
+    var _banda_t_holes = [];
+
+    for(Vector2 pos in hole_pos) {
+      Banda_T_Hole _banda_t_hole = Banda_T_Hole();
+      _banda_t_hole.position = pos;
+      _banda_t_holes.add(_banda_t_hole);
+    }
+
+    for (Banda_T_Hole _hole in _banda_t_holes) {
+      await add(_hole);
+    }
+
+    await add(_basureros);
+    await add(_bp);
+  }
+}

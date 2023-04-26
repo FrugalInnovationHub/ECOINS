a63c3f8c41222fa0cb335a6cac9a496c4f4d28d4
Atharva Kukade <atharva.kukade@icloud.com>
2023-04-25 21:04:49 -0700

    Blicking effect on the last level added

--- lib/palette.dart
+++ lib/palette.dart
@@ -3,5 +3,5 @@ import 'dart:ui';
 import 'package:flame/palette.dart';
 
 class Palette {
-  static PaletteEntry transp = BasicPalette.green;
+  static PaletteEntry transp = BasicPalette.transparent;
 }

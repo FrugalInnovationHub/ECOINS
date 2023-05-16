f368ea231f58951730262d7f4bdfc80d491a36cc
Atharva Kukade <atharva.kukade@icloud.com>
2023-05-16 11:04:00 -0700

    Merged with resizing and level3

--- lib/trash_items.dart
+++ lib/trash_items.dart
@@ -200,8 +200,8 @@ class Trash_Item extends SpriteComponent
                     gota_score_disp != null &&
                     sol_score_disp != null && score != null) {
                   blue_scored = true;
-                  score.updateScore(10);
-                  blue_score.blue_updateScore(10);
+                  score.updateScore(1);
+                  blue_score.blue_updateScore(1);
                   gota_score_disp.recycleLevel(1);
                   sol_score_disp.recycleLevel(1);
                   add(h_opacity_effect);
@@ -237,8 +237,8 @@ class Trash_Item extends SpriteComponent
                 if (yellow_score != null && gota_score_disp != null &&
                 sol_score_disp != null && score != null) {
                   yellow_scored = true;
-                  score.updateScore(10);
-                  yellow_score.updateScore(10);
+                  score.updateScore(1);
+                  yellow_score.updateScore(1);
                   gota_score_disp.recycleLevel(1);
                   sol_score_disp.recycleLevel(1);
                   add(h_opacity_effect);

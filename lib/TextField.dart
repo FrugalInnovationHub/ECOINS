e00d3b0579e354814e7da5e96af78098f6419cc3
Atharva Kukade <atharva.kukade@icloud.com>
2023-05-27 18:09:39 -0700

    Added enter button

--- lib/TextField.dart
+++ lib/TextField.dart
@@ -40,30 +40,49 @@ class _TextFieldExampleState extends State<TextFieldExample>{
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Container(
-        width: MediaQuery.of(context).size.width/4.5,
-        height: MediaQuery.of(context).size.width/30,
-        child: TextField(
-          controller: _controller,
-          onSubmitted: (String value) async {
-            await showDialog<void>(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('Thanks!'),
-                  actions: <Widget>[
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text('OK'),
-                    ),
-                  ],
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width / 4.5,
+            height: MediaQuery.of(context).size.width / 30,
+      margin: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width*0.60, 0, 0, MediaQuery.of(context).size.height*0.09),
+      padding: EdgeInsets.all(4),
+            child: TextField(
+              controller: _controller,
+              onSubmitted: (String value) async {
+                await showDialog<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Thanks!'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('OK'),
+                        ),
+                      ],
+                    );
+                  },
                 );
               },
-            );
-          },
-        ),
+            ),
+          ),
+          SizedBox(height:1, width: 1,), // Add some spacing between the TextField and FlatButton
+          InkWell(
+            onTap: () {}, // Image tapped
+            splashColor: Colors.white10, // Splash color over image
+            child: Ink.image(
+              fit: BoxFit.fill, // Fixes border issues
+              width: 120,
+              height: 50,
+              image: AssetImage(
+                  'images/enter_button.png',
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

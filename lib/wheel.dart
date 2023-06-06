3c6466738823f896023d424eee8656183ceeb48b
Tanmay Singla <tanmay1903@hotmail.com>
2023-06-06 12:54:30 -0700

    wheels fixed

--- lib/wheel.dart
+++ lib/wheel.dart
@@ -14,7 +14,7 @@ class Wheel extends PositionComponent with HasGameRef {
     // sprite = await gameRef.loadSprite('Tuerca_transparencia.gif');
     // sprite = await gameRef.loadSprite('croppedWheel.gif');
     final imageInstance = await Flame.images.load('wheelSprite.png');
-    final spriteSheet = SpriteSheet(image: imageInstance, srcSize: Vector2(248.5, 245));
+    final spriteSheet = SpriteSheet(image: imageInstance, srcSize: Vector2((imageInstance.width/50).toDouble(), imageInstance.height.toDouble()));
     final animation = spriteSheet.createAnimation(row: 0, stepTime: 0.1, loop: true);
     final animationComponent = SpriteAnimationComponent(
       animation: animation,

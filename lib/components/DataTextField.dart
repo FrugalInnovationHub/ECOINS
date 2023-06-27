ee2e0bb6901890680042e26a390a2e59c96b34ac
Tanmay Singla <tanmay1903@hotmail.com>
2023-06-27 13:33:43 -0700

    Drop downs added

--- lib/components/DataTextField.dart
+++ lib/components/DataTextField.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
+import 'package:ecoins/components/CustomSelectDropDown.dart';
 
 class DataTextField extends StatefulWidget {
   final game;
@@ -15,12 +16,74 @@ class _DataTextFieldState extends State<DataTextField> {
   late TextEditingController _country_controller;
   late TextEditingController _gender_controller;
 
+  var countryList = [
+    "Argentina",
+    "Bolivia",
+    "Brasil",
+    "Chile",
+    "Colombia",
+    "Costa Rica",
+    "Cuba",
+    "Ecuador",
+    "El Salvador",
+    "España",
+    "Estados Unidos de América",
+    "Guatemala",
+    "Honduras",
+    "México",
+    "Nicaragua",
+    "Panamá",
+    "Paraguay",
+    "Perú",
+    "Puerto Rico",
+    "República Dominicana",
+    "Uruguay",
+    "Venezuela",
+    "Otro"
+  ];
+
+  var genderList = [
+    "Masculino",
+    "Femenino",
+    "Otro",
+    "No respondo"
+  ];
+
+  var ageList = ["1"];
+
+  var countryValue;
+  var genderValue;
+  var ageValue;
+
+  void onValueChanged (type, value) {
+    if(type == "country") {
+      setState(() {
+        countryValue = value;
+      });
+    }
+    if(type == "gender") {
+      setState(() {
+        genderValue = value;
+      });
+    }
+    if(type == "age") {
+      setState(() {
+        ageValue = value;
+      });
+    }
+  }
   @override
   void initState() {
     super.initState();
     _age_controller = TextEditingController();
     _country_controller = TextEditingController();
     _gender_controller = TextEditingController();
+    countryValue = null;
+    genderValue = null;
+    for (int i = 2; i <= 100; i++) {
+      ageList.add(i.toString());
+    }
+    ageValue = null;
   }
 
   @override
@@ -30,6 +93,7 @@ class _DataTextFieldState extends State<DataTextField> {
     _gender_controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,114 +116,169 @@ class _DataTextFieldState extends State<DataTextField> {
                       SizedBox(height: constraints.maxHeight*0.05),
                       Container(
                         width: constraints.maxWidth*0.25,
-                        height: constraints.maxHeight*0.04,
+                        height: constraints.maxHeight*0.043,
                         margin: EdgeInsets.fromLTRB(constraints.maxWidth*0.1, 0, 0, 0),
-                        padding: EdgeInsets.fromLTRB(0,0,0,8),
-                        // color: Colors.green,
-                        child: TextField(
-                          style: TextStyle(
+                        // padding: EdgeInsets.fromLTRB(0,0,0,8),
+                        child: CustomSelectDropDown(
+                          itemList: countryList,
+                          dropdownValue: countryValue,
+                          onValueChanged: onValueChanged,
+                          type: "country",
+                          textStyle: TextStyle(
                               color: Colors.blue[700],
                               fontSize: constraints.maxHeight*0.025,
                               fontWeight: FontWeight.bold
                           ),
-                          decoration: InputDecoration(
-                              border: InputBorder.none
-                          ),
-                          controller: _country_controller,
-                          onSubmitted: (String value) async {
-                            await showDialog<void>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: const Text('Thanks!'),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: const Text('OK'),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
+                          constraints: constraints,
                         ),
                       ),
+
+                      // Container(
+                      //   width: constraints.maxWidth*0.25,
+                      //   height: constraints.maxHeight*0.04,
+                      //   margin: EdgeInsets.fromLTRB(constraints.maxWidth*0.1, 0, 0, 0),
+                      //   padding: EdgeInsets.fromLTRB(0,0,0,8),
+                      //   // color: Colors.green,
+                      //   child: TextField(
+                      //     style: TextStyle(
+                      //         color: Colors.blue[700],
+                      //         fontSize: constraints.maxHeight*0.025,
+                      //         fontWeight: FontWeight.bold
+                      //     ),
+                      //     decoration: InputDecoration(
+                      //         border: InputBorder.none
+                      //     ),
+                      //     controller: _country_controller,
+                      //     onSubmitted: (String value) async {
+                      //       await showDialog<void>(
+                      //         context: context,
+                      //         builder: (BuildContext context) {
+                      //           return AlertDialog(
+                      //             title: const Text('Thanks!'),
+                      //             actions: <Widget>[
+                      //               TextButton(
+                      //                 onPressed: () {
+                      //                   Navigator.pop(context);
+                      //                 },
+                      //                 child: const Text('OK'),
+                      //               ),
+                      //             ],
+                      //           );
+                      //         },
+                      //       );
+                      //     },
+                      //   ),
+                      // ),
                       SizedBox(height: constraints.maxHeight*0.04),
                       Container(
                         width: constraints.maxWidth*0.25,
-                        height: constraints.maxHeight*0.04,
+                        height: constraints.maxHeight*0.043,
                         margin: EdgeInsets.fromLTRB(constraints.maxWidth*0.1, 0, 0, 0),
-                        padding: EdgeInsets.fromLTRB(0,0,0,8),
-                        // color: Colors.green,
-                        child: TextField(
-                          style: TextStyle(
+                        // padding: EdgeInsets.fromLTRB(0,0,0,8),
+                        child: CustomSelectDropDown(
+                          itemList: ageList,
+                          dropdownValue: ageValue,
+                          onValueChanged: onValueChanged,
+                          type: "age",
+                          textStyle: TextStyle(
                               color: Colors.blue[700],
                               fontSize: constraints.maxHeight*0.025,
                               fontWeight: FontWeight.bold
                           ),
-                          decoration: InputDecoration(
-                              border: InputBorder.none
-                          ),
-                          controller: _age_controller,
-                          onSubmitted: (String value) async {
-                            await showDialog<void>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: const Text('Thanks!'),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: const Text('OK'),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
+                          constraints: constraints,
                         ),
                       ),
+                      // Container(
+                      //   width: constraints.maxWidth*0.25,
+                      //   height: constraints.maxHeight*0.04,
+                      //   margin: EdgeInsets.fromLTRB(constraints.maxWidth*0.1, 0, 0, 0),
+                      //   padding: EdgeInsets.fromLTRB(0,0,0,8),
+                      //   // color: Colors.green,
+                      //   child: TextField(
+                      //     style: TextStyle(
+                      //         color: Colors.blue[700],
+                      //         fontSize: constraints.maxHeight*0.025,
+                      //         fontWeight: FontWeight.bold
+                      //     ),
+                      //     decoration: InputDecoration(
+                      //         border: InputBorder.none
+                      //     ),
+                      //     controller: _age_controller,
+                      //     onSubmitted: (String value) async {
+                      //       await showDialog<void>(
+                      //         context: context,
+                      //         builder: (BuildContext context) {
+                      //           return AlertDialog(
+                      //             title: const Text('Thanks!'),
+                      //             actions: <Widget>[
+                      //               TextButton(
+                      //                 onPressed: () {
+                      //                   Navigator.pop(context);
+                      //                 },
+                      //                 child: const Text('OK'),
+                      //               ),
+                      //             ],
+                      //           );
+                      //         },
+                      //       );
+                      //     },
+                      //   ),
+                      // ),
                       SizedBox(height: constraints.maxHeight*0.04),
                       Container(
                         width: constraints.maxWidth*0.25,
-                        height: constraints.maxHeight*0.04,
+                        height: constraints.maxHeight*0.043,
                         margin: EdgeInsets.fromLTRB(constraints.maxWidth*0.1, 0, 0, 0),
-                        padding: EdgeInsets.fromLTRB(0,0,0,8),
-                        // color: Colors.green,
-                        child: TextField(
-                          style: TextStyle(
+                        // padding: EdgeInsets.fromLTRB(0,0,0,8),
+                        child: CustomSelectDropDown(
+                          itemList: genderList,
+                          dropdownValue: genderValue,
+                          onValueChanged: onValueChanged,
+                          type: "gender",
+                          textStyle: TextStyle(
                               color: Colors.blue[700],
                               fontSize: constraints.maxHeight*0.025,
                               fontWeight: FontWeight.bold
                           ),
-                          decoration: InputDecoration(
-                              border: InputBorder.none
-                          ),
-                          controller: _gender_controller,
-                          onSubmitted: (String value) async {
-                            await showDialog<void>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: const Text('Thanks!'),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: const Text('OK'),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
+                          constraints: constraints,
                         ),
                       ),
+                      // Container(
+                      //   width: constraints.maxWidth*0.25,
+                      //   height: constraints.maxHeight*0.04,
+                      //   margin: EdgeInsets.fromLTRB(constraints.maxWidth*0.1, 0, 0, 0),
+                      //   padding: EdgeInsets.fromLTRB(0,0,0,8),
+                      //   // color: Colors.green,
+                      //   child: TextField(
+                      //     style: TextStyle(
+                      //         color: Colors.blue[700],
+                      //         fontSize: constraints.maxHeight*0.025,
+                      //         fontWeight: FontWeight.bold
+                      //     ),
+                      //     decoration: InputDecoration(
+                      //         border: InputBorder.none
+                      //     ),
+                      //     controller: _gender_controller,
+                      //     onSubmitted: (String value) async {
+                      //       await showDialog<void>(
+                      //         context: context,
+                      //         builder: (BuildContext context) {
+                      //           return AlertDialog(
+                      //             title: const Text('Thanks!'),
+                      //             actions: <Widget>[
+                      //               TextButton(
+                      //                 onPressed: () {
+                      //                   Navigator.pop(context);
+                      //                 },
+                      //                 child: const Text('OK'),
+                      //               ),
+                      //             ],
+                      //           );
+                      //         },
+                      //       );
+                      //     },
+                      //   ),
+                      // ),
                       SizedBox(height: constraints.maxHeight*0.025),
                       InkWell(
                         child: Container(
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,21 +11,10 @@ import 'package:flame_audio/flame_audio.dart';
 class HomeScreen extends FlameGame with HasTappables {
 
   late BuildContext context;
-  // ImageSprite ecoins = ImageSprite();
-  // ImageSprite cangrejo2 = ImageSprite();
-  // ImageSprite cangrejo1 = ImageSprite();
-  // ImageSprite cangrejo3 = ImageSprite();
-  // ImageSprite dolphin1 = ImageSprite();
-  // ImageSprite dolphin2 = ImageSprite();
-  // ImageSprite girl = ImageSprite();
-  // ImageSprite whale = ImageSprite();
-  // ImageSprite turtle = ImageSprite();
-  // ImageSprite pelican = ImageSprite();
   ImageSprite homeScreen1 = ImageSprite();
   ImageSprite homeScreen2 = ImageSprite();
   late PlayButton button;
   late TutorialAnimation animation;
-  // late PolygonComponent triangle;
 
   Paint blue = Paint()
     ..color = const Color(0xFF3A9BDC);
@@ -38,14 +27,6 @@ class HomeScreen extends FlameGame with HasTappables {
   Color backgroundColor() => const Color(0xFFFFFFFF);
 
   void onClick() async {
-    // animation = TutorialAnimation(
-    //     text_url: "INTRO_1.png",
-    //     position: Vector2(size[0]*0.3, size[1]*0.01),
-    //     size: Vector2(size[0]*0.5, size[1]*0.2),
-    //     audio_url: "INTRO_1.mp3",
-    //     align: "left",
-    //     is_girl_image: false);
-    // add(animation);
     homeScreen2 = ImageSprite(asset: 'HomeScreen_2.jpeg',
         position: Vector2(0,0),
         size: Vector2(size[0], size[1])
@@ -68,17 +49,6 @@ class HomeScreen extends FlameGame with HasTappables {
     add(data);
     homeScreen1.removeFromParent();
     overlays.add("EnterData");
-    // Navigator.pushReplacementNamed(this.context, '/video');
-  //   ecoins.addOpacity();
-  //   pelican.addOpacity();
-  //   whale.addOpacity();
-  //   dolphin1.addOpacity();
-  //   dolphin2.addOpacity();
-  //   triangle.add(h_opacity_effect);
-  //   data = ImageSprite(asset: 'data.png', position: Vector2(size[0] * 0.25, size[1] * 0.275), size: Vector2(size[0] * 0.5, size[1] * 0.4));
-  //   Future.delayed(Duration(seconds: 1) ,() => add(data));
-  //   overlays.add("EnterData");
-
   }
 
   @override
@@ -86,39 +56,6 @@ class HomeScreen extends FlameGame with HasTappables {
     super.onLoad();
     var y = (size[0] / 16)*9;
     camera.viewport = FixedResolutionViewport(Vector2(size[0], y));
-    // ecoins = ImageSprite(asset: 'ecoins_logo.png',
-    //     position: Vector2(size[0] * 0.31, size[1] * 0.34),
-    //     size: Vector2(size[0] * 0.37, size[1] * 0.27));
-    // cangrejo2 = ImageSprite(asset: 'cangrejo.png',
-    //     position: Vector2(size[0]*0.18, size[1] * 0.75),
-    //     size: Vector2(size[0] * 0.1, size[1] * 0.17));
-    // cangrejo1 = ImageSprite(asset: 'cangrejo.png',
-    //     position: Vector2(size[0] * 0.045, size[1] * 0.75),
-    //     size: Vector2(size[0] * 0.08, size[1] * 0.13));
-    // cangrejo3 = ImageSprite(asset: 'cangrejo.png',
-    //     position: Vector2(size[0] * 0.88, size[1] * 0.82),
-    //     size: Vector2(size[0] * 0.09, size[1] * 0.15));
-    // dolphin1 = ImageSprite(asset: 'DOLPHIN.png',
-    //     position: Vector2(size[0] * 0.08, size[1] * 0.22),
-    //     size: Vector2(size[0] * 0.06, size[1] * 0.32),
-    //     angle: -2);
-    // dolphin2 = ImageSprite(asset: 'DOLPHIN.png',
-    //     position: Vector2(size[0] * 0.30, size[1] * 0.37),
-    //     size: Vector2(size[0] * 0.05, size[1] * 0.25),
-    //     angle: -2);
-    // girl = ImageSprite(asset: 'GIRL.png',
-    //     position: Vector2(size[0] * 0.75, size[1] * 0.11),
-    //     size: Vector2(size[0] * 0.2, size[1] * 0.87));
-    // whale = ImageSprite(asset: 'WHALE.png',
-    //     position: Vector2(-(size[0] * 0.08), size[1] * 0.98),
-    //     size: Vector2(size[0] * 0.18, size[1] ),
-    //     angle: -2.25);
-    // turtle = ImageSprite(asset: 'Turtle.png',
-    //     position: Vector2(size[0] * 0.64, size[1] * 0.80),
-    //     size: Vector2(size[0] * 0.2, size[1] * 0.18));
-    // pelican = ImageSprite(asset: 'Pelican.png',
-    //     position: Vector2(size[0] * 0.50, size[1] * 0.26),
-    //     size: Vector2(size[0] * 0.08, size[1] * 0.20));
 
     homeScreen1 = ImageSprite(asset: 'HomeScreen_1.jpeg',
       position: Vector2(0,0),
@@ -134,32 +71,5 @@ class HomeScreen extends FlameGame with HasTappables {
 
     await add(button);
     await add(homeScreen1);
-
-    // button = PlayButton(radius: size[0] * 0.037,
-    //     position: Vector2(size[0] * 0.55, size[1] * 0.55),
-    //     paint: blue,
-    //     context: this.context);
-    // triangle = PolygonComponent([
-    //   Vector2(0, 0),
-    //   Vector2(size[0] * 0.037, size[1] * 0.035),
-    //   Vector2(0, size[1] * 0.07)
-    // ], position: Vector2(size[0] * 0.575, size[1] * 0.58),
-    //     paint: green,
-    //     size: Vector2(size[0] * 0.12, size[1] * 0.22));
-
-
-    // await add(ecoins);
-    // await add(cangrejo1);
-    // await add(cangrejo2);
-    // await add(cangrejo3);
-    // await add(dolphin1);
-    // await add(dolphin2);
-    // await add(turtle);
-    // await add(girl);
-    // await add(whale);
-    //
-    // await add(pelican);
-
-    // await add(triangle);
   }
 }
